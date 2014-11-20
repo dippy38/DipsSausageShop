6c8aff4d71de61a6efd0fa683392d491e4646116
Mandeep Jhutti <crazymon1@gmail.com>
2014-11-20 09:03:21 +0000

    Display Basket
    Working on getting the basket view to play nicely with LIstView.
    Currently doesn’t only a button,

--- app/src/main/java/com/example/jhutti/myapplication/DisplayBasketActivity.java
+++ app/src/main/java/com/example/jhutti/myapplication/DisplayBasketActivity.java
@@ -1,10 +1,12 @@
 package com.example.jhutti.myapplication;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,24 +14,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Menu;
 import android.view.MenuInflater;
-
+import android.widget.RelativeLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import com.paypal.android.sdk.payments.PayPalAuthorization;
+import java.util.ArrayList;
+import android.widget.Button;
 
 
-public class DisplayBasketActivity extends ActionBarActivity {
+public class DisplayBasketActivity extends ActionBarActivity{
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
 
-        // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(20);
-        textView.setText(Basket.displayBasket());
+        //TODO CHANGE TO LISTVIEW HERE???????
+        RelativeLayout myLayout = new RelativeLayout(this);
+
+        Button myButton = new Button(this);
+        myButton.setText("Press me");
+
+        RelativeLayout.LayoutParams buttonParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonParams.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        myLayout.addView(myButton, buttonParams);
+        setContentView(myLayout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Create the text view THIS WORKS!
+       // TextView textView = new TextView(this);
+       // textView.setTextSize(20);
+      //  textView.setText(Basket.displayBasket());
 
         // Set the text view as the activity layout
-        setContentView(textView);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      // setContentView(textView);
+      // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
@@ -71,10 +97,9 @@ public class DisplayBasketActivity extends ActionBarActivity {
         public PlaceholderFragment() { }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.activity_display_message,
-                    container, false);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_display_message, container, false);
+            System.out.println("here!!!!!!!!!!!!!!!!!!!!!!!");
             return rootView;
         }
     }
